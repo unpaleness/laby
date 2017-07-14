fdca549e489851e486531acc705f5a7fb3253fd4
Egor Morunov <emorunov@masterhost.ru>
2017-07-14 13:36:50 +0300

    Generates well.

--- src/laby.cpp
+++ src/laby.cpp
@@ -11,6 +11,7 @@
 Laby::Laby() {}
 
 Laby::Laby(int x, int y, ofstream *l, ostream *m) : x(x), y(y), log_stream(l), main_stream(m) {
+    *log_stream << "Info: " << __FUNCTION__ << "(): constructing..." << endl;
     if (x > 0 && y > 0) {
         walls_v = new bool *[y];
         for (int i = 0; i < y; ++i) {
@@ -28,6 +29,7 @@ Laby::Laby(int x, int y, ofstream *l, ostream *m) : x(x), y(y), log_stream(l), m
 }
 
 Laby::~Laby() {
+    *log_stream << "Info: " << __FUNCTION__ << "(): destructing..." << endl;
     if (walls_v != nullptr) {
         *log_stream << "Info: " << __FUNCTION__ << "(): deleting vertical walls" << endl;
         for (int i = 0; i < y; ++i) {
@@ -48,11 +50,11 @@ Laby::~Laby() {
 }
 
 int Laby::print() {
+    *log_stream << "Info: " << __FUNCTION__ << "(): printing" << endl;
     if (walls_v == nullptr || walls_h == nullptr) {
         *log_stream << "Warning: " << __FUNCTION__ << "(): walls not initialized" << endl;
         return 1;
     }
-    *log_stream << "Info: " << __FUNCTION__ << "(): printing" << endl;
     for (int j = 0; j < y + 1; ++j) {
         // Upper corners and upper borders
         for (int i = 0; i < x; ++i) {
@@ -81,19 +83,20 @@ int Laby::print() {
             *main_stream << endl;
         }
     }
+    *log_stream << "Info: " << __FUNCTION__ << "(): printing complete" << endl;
     return 0;
 }
 
-/****************\
-* PUBLIC METHODS *
-\****************/
+/*****************\
+* PRIVATE METHODS *
+\*****************/
 
 int Laby::generate() {
+    *log_stream << "Info: " << __FUNCTION__ << "(): generating..." << endl;
     if (walls_v == nullptr || walls_h == nullptr) {
         *log_stream << "Warning: " << __FUNCTION__ << "(): cannot generate, walls not initialized" << endl;
         return 1;
     }
-    *log_stream << "Info: " << __FUNCTION__ << "(): generating" << endl;
 
     // Edge borders
     for (int i = 0; i < x; ++i) {
@@ -104,7 +107,14 @@ int Laby::generate() {
         walls_v[i][0] = true;
         walls_v[i][x] = true;
     }
-    // First row
+    // In this case generation is not needed
+    if (x < 2 || y < 2) {
+        *log_stream << "Info: " << __FUNCTION__ << "(): generating complete (too small)" << endl;
+        return 0;
+    }
+    /***********\
+    * First row *
+    \***********/
     int row[x];
     int n_multi { 1 }; // Index of the last multiplicity created
     for (int i = 0; i < x; ++i) {
@@ -117,25 +127,6 @@ int Laby::generate() {
         walls_h[1][i] = rand_bool();
         row[i] = n_multi;
     }
-    *main_stream << '#';
-    for (int i = 0; i < x; ++i) {
-        *main_stream << row[i];
-        if (walls_v[0][i + 1]) {
-            *main_stream << '#';
-        } else {
-            *main_stream << ' ';
-        }
-    }
-    *main_stream << endl << '#';
-    for (int i = 0; i < x; ++i) {
-        if (walls_h[1][i]) {
-            *main_stream << '#';
-        } else {
-            *main_stream << ' ';
-        }
-        *main_stream << '#';
-    }
-    *main_stream << endl;
     set <int> multies (row, row + x); // A set of multies on current row
     map <int, int> multi_cells;       // Amount of cells of each multi
     map <int, int> multi_borders;     // Amount of lower borders of each multi
@@ -150,12 +141,10 @@ int Laby::generate() {
             ++multi_borders[row[i]];
         }
     }
-    // If amount of lower borders as equal to cells of each multi delete one
+    // If amount of lower borders is equal to cells of each multi delete one
     // single random lower border of each multi
     for (auto m : multies) {
-        *main_stream << m << ' ' << multi_cells[m] << ' ' << multi_borders[m] << endl;
         if (multi_borders[m] == multi_cells[m]) {
-            *main_stream << "Multiplicity " << m << " is now trapped" << endl;
             int i_deleting_border { rand() % multi_borders[m] + 1 };
             int i_border { 0 };
             for (int i = 0; i < x; ++i) {
@@ -164,13 +153,114 @@ int Laby::generate() {
                 }
                 if (i_border == i_deleting_border) {
                     walls_h[1][i] = false;
-                    *main_stream << "Deleting wall under " << i + 1 << " cell" << endl;
                     break;
                 }
             }
         }
     }
-
+    /**************\
+    * 2 - y-1 rows *
+    \**************/
+    for (int j = 1; j < y - 1; ++j) {
+        // Set new multies to the cells with borders above
+        for (int i = 0; i < x; ++i) {
+            if (walls_h[j][i]) {
+                row[i] = ++n_multi;
+            }
+        }
+        // Set vertical borders
+        for (int i = 0; i < x - 1; ++i) {
+            if (row[i] == row[i + 1]) {
+                walls_v[j][i + 1] = true;
+            } else {
+                walls_v[j][i + 1] = rand_bool();
+                // All cells with the same multi should be changed
+                if (!walls_v[j][i + 1]) {
+                    int multi_to_merge { row[i + 1] };
+                    for (int k = 0; k < x; ++k) {
+                        if (row[k] == multi_to_merge) {
+                            row[k] = row[i];
+                        }
+                    }
+                }
+            }
+        }
+        // Set horizontal borders
+        for (int i = 0; i < x; ++i) {
+            walls_h[j + 1][i] = rand_bool();
+        }
+        multies.clear();
+        for (int i = 0; i < x; ++i) {
+            multies.insert(row[i]);
+        }
+        for (auto m : multies) {
+            multi_cells[m] = 0;
+            multi_borders[m] = 0;
+        }
+        // Count cells and lower borders for each multi
+        for (int i = 0; i < x; ++i) {
+            ++multi_cells[row[i]];
+            if (walls_h[j + 1][i]) {
+                ++multi_borders[row[i]];
+            }
+        }
+        // If amount of lower borders as equal to cells of each multi delete one
+        // single random lower border of each multi
+        for (auto m : multies) {
+            if (multi_borders[m] == multi_cells[m]) {
+                int i_deleting_border { rand() % multi_borders[m] + 1 };
+                int i_border { 0 };
+                for (int i = 0; i < x; ++i) {
+                    if (row[i] == m) {
+                        ++i_border;
+                    }
+                    if (i_border == i_deleting_border) {
+                        walls_h[j + 1][i] = false;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    /**************\
+    * The last row *
+    \**************/
+    // Set new multies to the cells with borders above
+    for (int i = 0; i < x; ++i) {
+        if (walls_h[y - 1][i]) {
+            row[i] = ++n_multi;
+        }
+    }
+    // Set vertical borders
+    for (int i = 0; i < x - 1; ++i) {
+        if (row[i] == row[i + 1]) {
+            walls_v[y - 1][i + 1] = true;
+        } else {
+            walls_v[y - 1][i + 1] = rand_bool();
+            // All cells with the same multi should be changed
+            if (!walls_v[y - 1][i + 1]) {
+                int multi_to_merge { row[i + 1] };
+                for (int k = 0; k < x; ++k) {
+                    if (row[k] == multi_to_merge) {
+                        row[k] = row[i];
+                    }
+                }
+            }
+        }
+    }
+    // Remove borders between different multies and merge them
+    for (int i = 0; i < x - 1; ++i) {
+        if (row[i] != row[i + 1]) {
+            walls_v[y - 1][i + 1] = false;
+            int multi_to_merge { row[i + 1] };
+            for (int k = i + 1; k < x; ++k) {
+                if (row[k] == multi_to_merge) {
+                    row[k] = row[i];
+                }
+            }
+        }
+    }
+    *log_stream << "Info: " << __FUNCTION__ << "(): generating complete" << endl;
     return 0;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <cstring>
 #include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
@@ -9,12 +10,18 @@ using namespace std;
 
 int main(int argc, char **argv) {
     string name { argv[0] };
-    string log_name;
-    if (argc > 1) {
+    string log_name { "laby.log" };
+    int x { 0 };
+    int y { 0 };
+    if (argc > 3) {
         log_name = argv[1];
+        x = atoi(argv[2]);
+        y = atoi(argv[3]);
     } else {
         cerr << "Too few arguments" << endl;
         cerr << "1 argument: log file name" << endl;
+        cerr << "2 argument: width" << endl;
+        cerr << "3 argument: height" << endl;
         return 1;
     }
 
@@ -26,7 +33,7 @@ int main(int argc, char **argv) {
     }
 
     Laby *laby = nullptr;
-    laby = new Laby (8, 8, log, &cout);
+    laby = new Laby (x, y, log, &cout);
     laby->print();
 
     if (laby != nullptr) {
